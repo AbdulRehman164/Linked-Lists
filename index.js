6040f98f976cfc272425a1a9cc2fcd533ab5dbbe
Abdul Rehman <abdulrehmang571@gmail.com>
2023-09-17 07:23:42 +0500

    added LinkedList class and wrote append method in it

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ class Node {
     this.next = next;
   }
 }
+
+class LinkedList {
+  append(value) {
+    const node = new Node(value);
+    if (Object.keys(this).length === 0) this.head = node;
+    else {
+      let tmp = this.head;
+      while (tmp.next !== null) {
+        tmp = tmp.next;
+      }
+      tmp.next = node;
+    }
+  }
+}
+const list = new LinkedList();
+list.append(15);
+console.log(list);
+
+list.append(100);
+console.log(list);
+
+list.append(120);
+console.log(list);
